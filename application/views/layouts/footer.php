ca5d03fd6672f67e4ea69248f7e298597e822c3a
dimasabya <dimasabdulyasir9@gmail.com>
2024-08-27 22:38:44 +0700

    update salin nama file dan js di footer

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -1,3 +1,25 @@
+<script>
+    // Mendapatkan elemen input "Balancing Program"
+    const programInput = document.getElementById('program');
+
+    // Menambahkan event listener untuk klik pada input
+    programInput.addEventListener('click', () => {
+        // Membuka dialog file
+        const fileInput = document.createElement('input');
+        fileInput.type = 'file';
+
+        fileInput.onchange = () => {
+            // Mendapatkan nama file yang dipilih
+            const fileName = fileInput.files[0].name;
+            // Memasukkan nama file ke dalam input "Balancing Program"
+            programInput.value = fileName;
+        };
+
+        // Memicu klik pada input file (membuka dialog file)
+        fileInput.click();
+    });
+</script>
+
 </body>
 
 </html>
